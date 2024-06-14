3d2a38744bf4af725398757b0ff02c1254a51f7d
Marvin Kabira <marvobd@gmail.com>
2024-06-14 17:17:55 +0300

    completed lab

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,7 +6,16 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {projects.map((projects) => (
+          <ProjectItem 
+            key={projects.id}
+            name={projects.name}
+            about={projects.about}
+            technologie={projects.technologies}
+          />
+        ))}
+      </div>
     </div>
   );
 }
